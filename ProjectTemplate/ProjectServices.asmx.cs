e5bd8c2674ce5f97b13a9abdeec7eaba774ce61b
ChrisDolton <ddolton@asu.edu>
2024-01-30 00:16:13 -0500

    created USF, Q, A feedback related WebMethods and put sample test code for them in index.html

--- ProjectTemplate/ProjectServices.asmx.cs
+++ ProjectTemplate/ProjectServices.asmx.cs
@@ -62,5 +62,136 @@ namespace ProjectTemplate
 				return "Something went wrong, please check your credentials and db name and try again.  Error: "+e.Message;
 			}
 		}
-	}
+
+        [WebMethod(EnableSession = true)]
+        public int UnsolicitedFeedback(string userID, string problemArea, string complaint, string proposedSolution)
+        {
+			int unsolicitedFeedbackID = -333;
+            string sqlSelect = "insert into unsolicitedFeedback (problemArea, complaint, proposedSolution, submittedBy) " +
+                "values(@problemAreaValue, @complaintValue, @proposedSolutionValue, @idValue); SELECT LAST_INSERT_ID();";
+
+            MySqlConnection sqlConnection = new MySqlConnection(getConString());
+            MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+            sqlCommand.Parameters.AddWithValue("@problemAreaValue", HttpUtility.UrlDecode(problemArea));
+            sqlCommand.Parameters.AddWithValue("@complaintValue", HttpUtility.UrlDecode(complaint));
+            sqlCommand.Parameters.AddWithValue("@proposedSolutionValue", HttpUtility.UrlDecode(proposedSolution));
+            sqlCommand.Parameters.AddWithValue("@idValue", HttpUtility.UrlDecode(userID));
+
+            sqlConnection.Open();
+            try
+            {
+                unsolicitedFeedbackID = Convert.ToInt32(sqlCommand.ExecuteScalar());
+				
+                //if the query worked, unsolicitedFeedbackID will have the value of the primary key of the
+				//row we just inserted into the unsolicitedFeedback table
+            }
+            catch (Exception e)
+            {
+				unsolicitedFeedbackID = -1;
+                //something went wrong, so we don't have an unsolicitedFeedbackID to work with
+                //so we need to set it to something that we can check for later
+                //in this case, -1 will (hopefully) never be a valid unsolicitedFeedbackID
+                //so we can check for that later when this method is called
+            }
+            sqlConnection.Close();
+			return unsolicitedFeedbackID;
+			//return value will be the unsolicitedFeedbackID of the row we just inserted.  If the insert failed,
+			//it will be -1 instead.  This value can be used to display the feedback back to the user after
+			//they submit it, or to display a message if the submission failed.
+        }
+
+        [WebMethod(EnableSession = true)]
+        public int SubmitQuestion(string userID, string questionText, string daysToLive)
+        {
+            int questionID = -333;
+            int daysToLiveInt = 0;
+            try
+            {
+                daysToLiveInt = Convert.ToInt32(daysToLive);
+            }
+            catch (Exception e)
+            {
+                daysToLiveInt = 0;
+            }
+            if (daysToLiveInt <= 0)
+            {
+                daysToLiveInt = 31;
+            }
+
+            string sqlSelect = "insert into questions (questionText, expiryDate, submittedBy) " +
+                "values(@questionTextValue, @expiryDateValue, @idValue); SELECT LAST_INSERT_ID();";
+            DateTime thisDay = DateTime.Today;
+            DateTime expiryDate = thisDay.AddDays(daysToLiveInt);
+            string expiryDateStr = expiryDate.ToString("yyyy-MM-dd");
+
+
+            MySqlConnection sqlConnection = new MySqlConnection(getConString());
+            MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+            sqlCommand.Parameters.AddWithValue("@questionTextValue", HttpUtility.UrlDecode(questionText));
+            sqlCommand.Parameters.AddWithValue("@expiryDateValue", HttpUtility.UrlDecode(expiryDateStr));
+            sqlCommand.Parameters.AddWithValue("@idValue", HttpUtility.UrlDecode(userID));
+
+            sqlConnection.Open();
+            try
+            {
+                questionID = Convert.ToInt32(sqlCommand.ExecuteScalar());
+
+                //if the query worked, questionID will have the value of the primary key of the
+                //row we just inserted into the unsolicitedFeedback table
+            }
+            catch (Exception e)
+            {
+                questionID = -1;
+                //something went wrong, so we don't have a questionID to work with
+                //so we need to set it to something that we can check for later
+                //in this case, -1 will (hopefully) never be a valid questionID
+                //so we can check for that later when this method is called
+            }
+            sqlConnection.Close();
+            return questionID;
+            //return value will be the questionID of the row we just inserted.  If the insert failed,
+            //it will be -1 instead.  This value can be used to display the question back to the user after
+            //they submit it, or to display a message if the submission failed.
+        }
+
+        [WebMethod(EnableSession = true)]
+        public int SubmitAnswer(string userID, string answerText, string questionID)
+        {
+            int answerID = -333;
+            int questionIDInt = Convert.ToInt32(questionID);
+            string sqlSelect = "insert into answers (feedback, submittedBy, question) " +
+                "values(@feedbackValue, @idValue, @questionValue); SELECT LAST_INSERT_ID();";
+
+            MySqlConnection sqlConnection = new MySqlConnection(getConString());
+            MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+            sqlCommand.Parameters.AddWithValue("@feedbackValue", HttpUtility.UrlDecode(answerText));
+            sqlCommand.Parameters.AddWithValue("@idValue", HttpUtility.UrlDecode(userID));
+            sqlCommand.Parameters.AddWithValue("@questionValue", questionIDInt);
+
+            sqlConnection.Open();
+            try
+            {
+                answerID = Convert.ToInt32(sqlCommand.ExecuteScalar());
+
+                //if the query worked, answerID will have the value of the primary key of the
+                //row we just inserted into the unsolicitedFeedback table
+            }
+            catch (Exception e)
+            {
+                answerID = -1;
+                //something went wrong, so we don't have a answerID to work with
+                //so we need to set it to something that we can check for later
+                //in this case, -1 will (hopefully) never be a valid answerID
+                //so we can check for that later when this method is called
+            }
+            sqlConnection.Close();
+            return answerID;
+            //return value will be the answerID of the row we just inserted.  If the insert failed,
+            //it will be -1 instead.  This value can be used to display the answer back to the user after
+            //they submit it, or to display a message if the submission failed.
+        }
+    }
 }
