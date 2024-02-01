df0a9ae6aee21603235ccbe4b22974b686ea28b0
ncarder1 <ncarder1@asu.edu>
2024-02-01 00:59:47 -0800

    added post-questions, fdback, and separated files

---
+++ ProjectTemplate/post-questions.js
@@ -0,0 +1,12 @@
+﻿document.getElementById('postQuestionForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    // Get form values
+    var question = document.getElementById('question').value;
+    var duration = document.getElementById('duration').value;
+
+    // Perform AJAX to submit the question
+    // Add your AJAX code here
+
+    alert('Question Posted Successfully!');
+});
